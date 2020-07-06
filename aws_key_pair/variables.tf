5155552f32df7b039a1dd16a3c5c10e7bcbfc3ab
Walter Traspadini <walter.traspadini@sorint.it>
2020-07-06 13:06:44 +0200

    Module aws_key_pair

---
+++ aws_key_pair/variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "environment" {
+  type        = string
+  description = "The name of the environment."
+}
+
+variable "key_name" {
+  type        = string
+  description = "The name of the pulic key"
+}
+
+variable "secret_name" {
+  type        = string
+  description = "The secret manager secret name"
+}
+
+variable "secret_key_name" {
+  type        = string
+  description = "The name of the key within the secret"
+}
+
+variable "version_stage" {
+  type        = string
+  description = "Specifies the secret version that you want to retrieve by the staging label attached to the version. "
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags, each pair."
+  default     = null
+}
