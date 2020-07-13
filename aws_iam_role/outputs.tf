45cbd142156bd2fba033c5fbadd4708c2809ac93
Walter Traspadini <walter.traspadini@sorint.it>
2020-07-13 12:51:04 +0200

    iam role instance profile name as output parameter

--- aws_iam_role/outputs.tf
+++ aws_iam_role/outputs.tf
@@ -9,3 +9,8 @@ output "arn" {
 output "name" {
   value = aws_iam_role.this.name
 }
+
+output "instance_profile_name" {
+  value       = var.instance_profile_name != null ? aws_iam_instance_profile.this[0].name : null
+  description = "The name of the instance profile."
+}
