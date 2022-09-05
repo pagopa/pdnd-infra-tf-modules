8e9aa9b177b3eb133ae069e1d31a6cdc133c1cfc
nablacito <alberto.gallinaro@audienceview.com>
2022-09-05 15:54:56 +0200

    fix: new synthax
    debug: route
    
    feat: ignore route tables
    
    fix: table association
    
    feat: table association
    
    fix: tables
    
    feat: association
    
    feat: scheduled-actions
    
    fix: resource naming
    
    debug: revert ignores

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -47,6 +47,10 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.this[count.index].id
   }
 
+  # lifecycle {
+  #   ignore_changes = ["route"]
+  # }
+
   tags = merge({
     Name        = "${var.vpc_name}-${var.environment}-public-rt"
     Environment = var.environment
@@ -110,9 +114,9 @@ resource "aws_route_table" "private" {
     gateway_id = "vgw-0678f7da286c28d58"
   }
 
-  
-
-
+  # lifecycle {
+  #   ignore_changes = ["route"]
+  # }
 
   tags = merge({
     Name        = "${var.vpc_name}-${var.environment}-private-rt-${count.index + 1}"
