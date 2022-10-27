9f3674e41473fdde3cae95b345d9dc98d9857f63
Christian <christian.cavuti@gmail.com>
2022-10-27 15:52:19 +0200

    fix: adapting code to old provider version

--- aws_vpc_endpoint/outputs.tf
+++ aws_vpc_endpoint/outputs.tf
@@ -1,15 +1,15 @@
 output "id" {
-    value = aws_vpc_endpoint.this.id
+  value = aws_vpc_endpoint.this.id
 }
 
 output "type" {
-    value = aws_vpc_endpoint.this.vpc_endpoint_type
+  value = aws_vpc_endpoint.this.vpc_endpoint_type
 }
 
 output "route_table_ids" {
-    value = aws_vpc_endpoint.this.route_table_ids
+  value = aws_vpc_endpoint.this.route_table_ids
 }
 
 output "service" {
-    value = aws_vpc_endpoint.this.service_name
+  value = aws_vpc_endpoint.this.service_name
 }
--- aws_vpc_endpoint/variables.tf
+++ aws_vpc_endpoint/variables.tf
@@ -1,19 +1,19 @@
 variable "vpc_id" {
-    type = string
-    description = "The ID of the VPC."
+  type        = string
+  description = "The ID of the VPC."
 }
 
 variable "service" {
-    type = string
-    description = "The service the VPC Endpoint applies to."
+  type        = string
+  description = "The service the VPC Endpoint applies to."
 }
 
 variable "type" {
-    type = string
-    description = "The VPC Endpoint type [Gateway, Interface]."
+  type        = string
+  description = "The VPC Endpoint type [Gateway, Interface]."
 }
 
 variable "route_table_ids" {
-    type = list(string)
-    description = "The Route Tables ids the VPC Endpoint applies to (only for Gateway endpoints)."
+  type        = list(string)
+  description = "The Route Tables ids the VPC Endpoint applies to (only for Gateway endpoints)."
 }
